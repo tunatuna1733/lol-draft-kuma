8429b220aab5a9c35fdbb6d9ab73d6e661f66274
tunatuna1733 <bouningen13@gmail.com>
2024-10-29 18:13:36 +0900

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { KumaRegistry } from '@kuma-ui/next-plugin/registry';
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -15,6 +16,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body>
+				<Analytics />
 				<KumaRegistry>{children}</KumaRegistry>
 			</body>
 		</html>
