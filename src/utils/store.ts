d0a0a38b4f97c44d0a3f5cf20b5ae79f4a897f0d
tunatuna1733 <bouningen13@gmail.com>
2024-11-03 20:23:41 +0900

    added icon and title

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const useStore = <T, F>(store: (callback: (state: T) => unknown) => unknown, callback: (state: T) => F) => {
 	const result = store(callback) as F;
